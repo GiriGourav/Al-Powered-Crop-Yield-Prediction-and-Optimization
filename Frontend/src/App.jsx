7adeb3c59a3165139751d83548e8150349ca3662
Raunak <raunaktomar30@gmail.com>
2025-09-17 22:46:49 +0530

    something removed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
 return (
